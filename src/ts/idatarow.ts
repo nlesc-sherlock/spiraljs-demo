4c9f6b031990ec7896aeb244fa846b3892c70c9f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-12-05 11:22:51 +0100

    initial commit is a copy of spiraljs (which includes the library and the demo files)

---
+++ src/ts/idatarow.ts
@@ -0,0 +1,11 @@
+
+interface IDataRow {
+    datestr          : string;
+    casenumber       : string;
+    dayOfWeek        : number;
+    description      : string;
+    primary          : string;
+    latitude         : number;
+    longitude        : number;
+    timeOfDay        : number;
+}
